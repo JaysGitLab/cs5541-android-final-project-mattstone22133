08f1ca72def624eff6b15509bf0e0920ba9bae3d
Matt Stone <Matt@Matts-MacBook-Air.local>
2016-11-10 09:42:41 -0500

    have programically set up stave with invisible bars, need to ensure notes have z values that put them in front of the bar

--- E-Z Notes/BarSprite.swift
+++ E-Z Notes/BarSprite.swift
@@ -9,6 +9,69 @@
 import UIKit
 import SpriteKit
 
-class BarSprite: SKSpriteNode {
+class BarSprite: SKNode {
+    var containedSprites:SKNode = SKNode() //this is treated like an linked list container
+    var numBarSegmentsNeeded:Int = 1
+    var imageName:String = "ez_bar_2x100.png" //store this in plist? makes it easy to change after-fact.
 
+    
+    //This constructor should not be used.
+    //instead use the constructo that specifies how large of a range the bar should cover.
+    override init(){
+        super.init()
+        
+        //set # bars needed to draw  (will be 1 for the no-arg)
+        createBars()
+        addChild(containedSprites)
+    }
+    
+    init(sizeBarNeedsToCover sizeNeeded:CGFloat){
+        super.init()
+        
+        //calculate how many bars will be needed to cover screen
+        calculateNumberBarsNeededForSize(sizeNeeded: sizeNeeded)
+        
+        //set bars needed
+        createBars()
+        addChild(containedSprites)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        //frameSize = aDecoder.decodeObject(forKey: "frameSize") as! CGSize
+        super.init(coder: aDecoder) //called after this object has be inits fields
+    }
+
+    override func encode(with aCoder: NSCoder) {
+        super.encode(with: aCoder) //called before this object encodes any fields
+        //aCoder.encode(frameSize, forKey:"frameSize") //example
+    }
+    
+    //Creates enough bar segments to cover the screen horrozontally.
+    func createBars(){
+        // loop (numBarSegmentsNeeded) times, add a bar segment and displace it based on i
+        for i in 0..<numBarSegmentsNeeded {
+            let tempSprite = SKSpriteNode(imageNamed: imageName)
+            tempSprite.position = CGPoint(x: tempSprite.size.width * CGFloat(i), y: 0)
+            containedSprites.addChild(tempSprite)
+        }
+    }
+    
+    //set the field numBarSegmentsNeeded to an integer representing how many bar sprites should be drawn to cover screen
+    func calculateNumberBarsNeededForSize(sizeNeeded:CGFloat){
+        //create a temporary sprite to extract its width programmatically.
+        let tempSprite = SKSpriteNode(imageNamed: imageName) //wasteful, find way to get img width from file without object creation?
+        
+        //extract image width from the sprite size.
+        let imageWidth = tempSprite.size.width
+        
+        //number of bar segments needed is how many times the bar image can fit in the view (ie do division)
+        numBarSegmentsNeeded = Int(ceil(sizeNeeded / imageWidth) + 1)  //add 1 to account imperfect image size
+    }
+    
+    //TODO: consider overriding the property setter and getter instead of using a raw function like this (override isHidden)
+    func setHidden(Hidden hidden:Bool){
+        for inode in (containedSprites.children) {
+            inode.isHidden = hidden
+        }
+    }
 }
--- E-Z Notes/E-Z Notes/Stave.swift
+++ E-Z Notes/E-Z Notes/Stave.swift
@@ -10,20 +10,59 @@ import UIKit
 import SpriteKit
 
 class Stave: SKNode {
-    var topBar:SKNode = SKNode()
-    var bar2:SKNode = SKNode()
-    var bar3:SKNode = SKNode()
-    var bar4:SKNode = SKNode()
-    var bottomBar: SKNode = SKNode()
+    var barContainer:SKNode = SKNode()
+    var screenHeight:CGFloat = 600  //this should be updated in init, this is set for testing purposes
+    var screenWidth:CGFloat = 400   //TODO change this, it is set for debuggin purposes
+    //let noteSpacing:CGFloat = 10.0  //adjust this to change distance between bars
+    var numberOfBars = 26   //this number includes hidden bars
     
     
-    override init(){
+    init(Height height:CGFloat, Width width:CGFloat){
         super.init()
+        self.screenHeight = height
+        self.screenWidth = width
+        addChild(barContainer)
+        buildBarsBottomUp()
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        //frameSize = aDecoder.decodeObject(forKey: "frameSize") as! CGSize //EXAMPLE
+        super.init(coder: aDecoder) //called after this object has be inits fields
     }
     
-   
+    override func encode(with aCoder: NSCoder) {
+        super.encode(with: aCoder) //called before this object encodes any fields
+        //aCoder.encode(frameSize, forKey:"frameSize") //EXAMPLE
+    }
+    
+    func buildBarsBottomUp(){
+        let noteSpacing = screenHeight * 0.6 / CGFloat(numberOfBars)
+        
+        for i in 0..<numberOfBars {
+            let tempSprite = BarSprite(sizeBarNeedsToCover: screenWidth)
+            tempSprite.position = CGPoint(x: 0, y: noteSpacing * CGFloat(i))
+            barContainer.addChild(tempSprite)
+            //update note values?
+            
+            //hide odd bars (or the very middle bar)
+            if (i % 2 == 1 || barShouldBeHidden(barNumber: i)){
+                tempSprite.setHidden(Hidden: true)
+            }
+        }
+    }
+    
+    //function checks the value of a barNumber to determine if it is a special bar and should be hidden
+    func barShouldBeHidden(barNumber:Int) -> Bool{
+        let middleBar:Int = 12
+        let hiddenBottonBar:Int = 0
+        let hiddenTopBar:Int = 24
+        
+        return (barNumber == middleBar
+            || barNumber == hiddenBottonBar
+            || barNumber == hiddenTopBar)
+        
+    }
+    
 }
